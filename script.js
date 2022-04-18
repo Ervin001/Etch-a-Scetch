65b58384cc39c495902b0060ed2b15e1003abfa4
Ervin Gonzalez <evgnz24@gmail.com>
2022-04-18 01:20:45 -0500

    Refactored the code, now the code will be able to change according to how many columns and rows the user will want. And also if a user doesn't choose an option it will default to a 9x9... still no user prompt for user to decide

--- script.js
+++ script.js
@@ -2,23 +2,39 @@
 const buttonEl = document.querySelector('button');
 const container = document.querySelector('.container');
 
-for (let i = 1; i <= 16; i++) {
-  const div = document.createElement('div');
+const starter = function (num) {
+  for (let i = 1; i <= (num === undefined ? 16 : num); i++) {
+    const div = document.createElement('div');
 
-  div.classList.add('square', `square--${i}`);
-  container.appendChild(div);
-}
+    div.classList.add('square', `square--${i}`);
+    container.appendChild(div);
+  }
+};
+
+container.style.gridTemplateColumns = null;
+container.style.gridTemplateRows = null;
+
 const squares = document.querySelector('.square');
-
 const div = document.querySelectorAll('.square');
 const nodeToArr = [...div];
 
-nodeToArr.forEach((e) =>
-  e.addEventListener('mousemove', function (mv) {
-    mv.target.classList.add('square-color');
-  })
-);
+const newGrid = function (number) {
+  const sqrNum = Math.sqrt(number);
+  starter(number);
+  console.log(sqrNum);
+  container.style['grid-template-columns'] = null;
+  container.style['grid-template-rows'] = null;
 
-buttonEl.addEventListener('click', function (i) {
-  nodeToArr.forEach((e) => e.classList.remove('square-color'));
-});
+  // debugger;
+  for (let i = 0; i < number; i++) {
+    container.style['grid-template-columns'] = `repeat(${sqrNum}, 100px)`;
+    container.style['grid-template-rows'] = `repeat(${sqrNum}, 100px)`;
+  }
+};
+// newGrid(4);
+newGrid(9);
+// newGrid(16);
+// newGrid(25);
+// newGrid(36);
+// newGrid(49);
+// newGrid(64);
