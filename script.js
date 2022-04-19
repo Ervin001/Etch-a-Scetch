a019f2b9dd26af7ed93f8c73629f57f48e119a1f
Ervin Gonzalez <evgnz24@gmail.com>
2022-04-19 01:04:18 -0500

    Changed the code completly, new code for generating a grid

--- script.js
+++ script.js
@@ -1,40 +1,17 @@
 'use strict';
 const buttonEl = document.querySelector('button');
-const container = document.querySelector('.container');
+const gridEl = document.querySelector('.grid');
 
-const starter = function (num) {
-  for (let i = 1; i <= (num === undefined ? 16 : num); i++) {
+const createGrid = () => {
+  for (let i = 0; i < 256; i++) {
     const div = document.createElement('div');
+    div.classList.add('cell');
+    div.addEventListener('mouseover', function (e) {
+      e.target.style.backgroundColor = 'black';
+    });
 
-    div.classList.add('square', `square--${i}`);
-    container.appendChild(div);
+    gridEl.appendChild(div);
   }
 };
 
-container.style.gridTemplateColumns = null;
-container.style.gridTemplateRows = null;
-
-const squares = document.querySelector('.square');
-const div = document.querySelectorAll('.square');
-const nodeToArr = [...div];
-
-const newGrid = function (number) {
-  const sqrNum = Math.sqrt(number);
-  starter(number);
-  console.log(sqrNum);
-  container.style['grid-template-columns'] = null;
-  container.style['grid-template-rows'] = null;
-
-  // debugger;
-  for (let i = 0; i < number; i++) {
-    container.style['grid-template-columns'] = `repeat(${sqrNum}, 100px)`;
-    container.style['grid-template-rows'] = `repeat(${sqrNum}, 100px)`;
-  }
-};
-// newGrid(4);
-newGrid(9);
-// newGrid(16);
-// newGrid(25);
-// newGrid(36);
-// newGrid(49);
-// newGrid(64);
+createGrid();
