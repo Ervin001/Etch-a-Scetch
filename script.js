b6a150360d0244151751e62f0890214d19457461
Ervin Gonzalez <evgnz24@gmail.com>
2022-04-19 22:08:44 -0500

    Finished the Project, added even listeners to each div inside the grid(seemed like the best way to go about it), Also added the option to switch between random colors and black without resetting the grid.

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 'use strict';
 const buttonEl = document.querySelector('.reset');
 const gridEl = document.querySelector('.grid');
+const blackBtnEl = document.querySelector('.black');
+const randomBtnEl = document.querySelector('.random');
 
+//Starting Conditions
 const createGrid = () => {
   for (let i = 0; i < 256; i++) {
     const div = document.createElement('div');
     div.classList.add('cell');
+    //Each div gets an event listener
     div.addEventListener('mouseover', function (e) {
       e.target.style.backgroundColor = '#000';
     });
@@ -14,6 +18,7 @@ const createGrid = () => {
   }
 };
 
+//Generates a random color
 const randomColor = function () {
   let maxVal = 0xffffff;
   let randomNumber = Math.random() * maxVal;
@@ -27,25 +32,61 @@ const randomColor = function () {
 
 //Slider
 const slider = document.getElementById('myRange');
-const output = document.querySelector('.sliderValue');
-output.innerHTML = slider.value;
+let outputVal = document.querySelector('.sliderValue');
 
-slider.oninput = function () {
-  output.innerHTML = this.value;
-};
+// Adjust grid size
+slider.addEventListener('input', function (e) {
+  let val = document.querySelector('#myRange').value;
+  outputVal.textContent = val;
+  removeChildNodes(gridEl);
+
+  for (let i = 0; i < val * val; i++) {
+    gridEl.setAttribute(
+      'style',
+      `grid-template-columns: repeat(${val}, 2fr); grid-template-rows: repeat(${val}, 2fr);`
+    );
+    const div = document.createElement('div');
+    div.classList.add('cell');
+    div.addEventListener('mouseover', function (e) {
+      e.target.style.backgroundColor = '#000';
+    });
+    gridEl.appendChild(div);
+  }
+});
+
+// Black button
+blackBtnEl.addEventListener('click', function () {
+  let val = document.querySelector('#myRange').value;
+  const cells = gridEl.childNodes;
+  for (let i = 0; i < val * val; i++) {
+    cells[i].addEventListener('mouseover', function (e) {
+      e.target.style.backgroundColor = '#000';
+    });
+  }
+});
+
+// Random button
+randomBtnEl.addEventListener('click', function () {
+  let val = document.querySelector('#myRange').value;
+  const cells = gridEl.childNodes;
+  for (let i = 0; i < val * val; i++) {
+    cells[i].addEventListener('mouseover', function (e) {
+      e.target.style.backgroundColor = randomColor();
+    });
+  }
+});
 
 // Remove the cells from the grid
 buttonEl.addEventListener('click', function (e) {
+  let val = document.querySelector('#myRange').value;
+  outputVal.textContent = val;
   removeChildNodes(gridEl);
+  gridEl.setAttribute(
+    'style',
+    `grid-template-columns: repeat(${val}, 2fr); grid-template-rows: repeat(${val}, 2fr);`
+  );
 
-  const cells = 9;
-
-  for (let i = 0; i < cells * cells; i++) {
-    gridEl.setAttribute(
-      'style',
-      `grid-template-columns: repeat(${cells}, 2fr); grid-template-rows: repeat(${cells}, 2fr);`
-    );
-
+  for (let i = 0; i < val * val; i++) {
     const div = document.createElement('div');
     div.classList.add('cell');
     div.addEventListener('mouseover', function (e) {
@@ -61,7 +102,6 @@ const removeChildNodes = function (parent) {
 
     while (gridEl.firstChild) {
       gridEl.removeChild(gridEl.firstChild);
-      // console.log(gridEl);
     }
     return children;
   }
